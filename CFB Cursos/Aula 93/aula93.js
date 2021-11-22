c2780c5f802cc6f2c3c83fee6bb81b88f1cedb57
AdemirRamos <ademirramos634@gmail.com>
2021-11-22 17:31:03 -0300

    Adição da Aula 93

---
+++ CFB Cursos/Aula 93/aula93.js
@@ -0,0 +1,12 @@
+var dv1
+window.addEventListener('load', iniciar)
+
+function iniciar() {
+    dv1 = document.getElementById('dv1')
+    dv1.addEventListener('click', trocar)
+}
+
+function trocar() {
+    var objeto = event.target //Buscar um substituto para "event".
+    objeto.sytle.backgroundImage = "url('Imagens/car3.jpg')"
+}
