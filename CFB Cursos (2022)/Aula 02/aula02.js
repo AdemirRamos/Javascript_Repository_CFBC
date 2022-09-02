2e78410ddaf5badb82544042be6931a615332b0e
AdemirRamos <ademirramos634@gmail.com>
2022-09-02 17:51:08 -0300

    Adição da Aula 02

---
+++ CFB Cursos (2022)/Aula 02/aula02.js
@@ -0,0 +1,15 @@
+//Aula 02 - Javascript em Modo Estrito, você sabe o que é?
+
+//O JS, em modo estrito, deixa o nosso código mais "limpo"; sem coisas inutilizadas; et cetera. (...)
+//Em modo estrito, não é possível utilizar uma variável não declarada, por exemplo.
+
+//Aplicando o modo estrito:
+
+"use strict"
+
+let nome = "Sofia"
+console.log(nome)
+
+//Caso a variável "nome" não seja declarada, ao executar o código acima, fora do modo estrito, não haverá problemas.
+//Dentro do modo estrito, ocorrerá um problema.
+//O modo estrito também conta com outras restrições.
