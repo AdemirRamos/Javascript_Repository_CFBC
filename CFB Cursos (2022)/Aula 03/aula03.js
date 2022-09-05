99be3c8106e35c5851ec0d69b143cd1755c4a016
AdemirRamos <ademirramos634@gmail.com>
2022-09-05 13:47:05 -0300

    Atualização da Aula 03
    As anotações (no documento) foram atualizadas.

--- CFB Cursos (2022)/Aula 03/aula03.js
+++ CFB Cursos (2022)/Aula 03/aula03.js
@@ -4,12 +4,12 @@
 
 var nome = "Sofia"
 
-//Uma variável é uma posição na memória RAM do computador.
-//As variáveis declaradas com "var", podem ser acessadas de qualquer escopo no programa e também podem ser alteradas.
-//A única exceção se dá com funções: uma "var", dentro de uma função, não pode ser acessada de fora dela.
-//Variáveis ocupam espaço (memória RAM)! Quanto mais variáveis, mais espaço será ocupado.
-//A memória RAM está ligada ao desempenho da máquina, logo, quanto menos memória RAM, menos desempenho.
-//Ao declarar uma variável com "var", o JS a aloca no topo do programa - tornando-a acessível em todo o programa.
+//Uma variável é uma posição na memória RAM do computador;
+//Variáveis ocupam espaço (memória RAM)! Quanto mais variáveis, mais espaço será ocupado;
+//A memória RAM está ligada ao desempenho da máquina, logo, quanto menos memória RAM disponível, menos desempenho;
+//Ao declarar uma variável com "var", o JS a eleva ao topo do escopo onde ela foi declarada;
+//Em razão disso, essa variável estará disponível para todos os escopos abaixo do escopo da variável em questão.
+//P. S.: Em funções, variáveis "var" só podem ser acessadas dentro do escopo da função (e não fora).
 
 if (true) {
 	var nome2 = "Ademir"
@@ -21,7 +21,7 @@ console.log(nome2) //A variável "nome2" ainda pode ser acessada de fora do esco
 
 //Já variáveis declaradas com "let" fazem com que essas variáveis sejam acessíveis somente do seu respectivo escopo.
 //P. S.: Essas variáveis também podem ser acessadas a partir dos escopos conseguintes.
-//Variáveis declaradas com "let" são elevadas ao topo do escopo onde foram declaradas.
+//Variáveis declaradas com "let" são elevadas ao topo do escopo onde foram declaradas;
 //Variáveis "let" também pode ter o seu valor alterado.
 
 function teste() {
@@ -33,6 +33,8 @@ function teste() {
 
 	console.log(`A variável "nome3" também é acessível daqui: ${nome3}.`)
 }
+
+//console.log(`A variável "nome3" não pode ser acessada deste escopo.`)
 
 teste()
 
@@ -47,4 +49,4 @@ console.log(dado)
 //Já variáveis declaradas com "const" não podem ter o seu valor alterado.
 
 const curso = "Javascript"
-console.log(número)
+console.log(curso)
