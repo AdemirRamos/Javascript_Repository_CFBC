e4a67d510c5c1945c00eeb5f70fb8f95f46a073a
AdemirRamos <ademirramos634@gmail.com>
2022-09-05 16:50:36 -0300

    Adição da Aula 08

---
+++ CFB Cursos (2022)/Aula 08/aula08.js
@@ -0,0 +1,33 @@
+//Aula 08 - Operadores Bitwise em Javascript
+
+//Para mais detalhes, ver aula 08.
+
+let n1 = 10
+let n2 = 11
+let resultado
+
+resultado = n1 & n2
+
+console.log(resultado)
+
+resultado = n1 | n2
+
+console.log(resultado)
+
+resultado = n1 ^ n2
+
+console.log(resultado)
+
+let n3 = 10
+
+resultado = n3 << 1
+
+console.log(resultado)
+
+resultado = n3 >> 1
+
+console.log(resultado)
+
+resultado = n3 << 2
+
+console.log(resultado)
