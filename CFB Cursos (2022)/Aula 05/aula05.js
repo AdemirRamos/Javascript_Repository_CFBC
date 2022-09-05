dc7de14dc1c7d863a4f60b73415ec99f12a3079f
AdemirRamos <ademirramos634@gmail.com>
2022-09-05 14:40:57 -0300

    Adição da Aula 05

---
+++ CFB Cursos (2022)/Aula 05/aula05.js
@@ -0,0 +1,16 @@
+//Aula 05 - Operadores Relacionais Em Javascript, Aprenda Como Usar!
+
+let número1 = 10, número2 = 5, número3 = 10
+
+console.log(número1 > número2)
+console.log(número1 < número2)
+console.log(número1 >= número3)
+console.log(número1 <= número3)
+console.log(número1 == número3)
+
+//A "!" (exclamação - chama-se "negação") inverte o resultado do teste lógico:
+
+console.log(!(número1 == número3))
+
+console.log(número1 != número3)
+console.log(!(número1 != número3))
