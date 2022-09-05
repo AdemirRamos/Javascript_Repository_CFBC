0ac50f033229923d79911f735ad789bf8c082be6
AdemirRamos <ademirramos634@gmail.com>
2022-09-05 17:06:29 -0300

    Adição da Aula 09

---
+++ CFB Cursos (2022)/Aula 09/aula09.js
@@ -0,0 +1,30 @@
+//Aula 09 - Diferença Entre Pré-incremento e Pós-incremento
+
+let n = 10
+
+console.log(n++) //Pós-incremento.
+console.log(++n) //Pré-incremento.
+
+n += 10
+
+console.log(n)
+
+console.log(n--) //Pós-incremento.
+console.log(--n) //Pré-incremento.
+
+//Operador de inversão (de valor - de positivo para negativo e vice-versa):
+
+n = -10
+
+console.log(n)
+
+let número = 10
+let x = -número
+
+console.log(x)
+
+let n1 = 10, n2 = 20
+
+console.log(n1 + n2)
+
+console.log(n1 + ' + ' + n2 + ' = ' + (n1 + n2)) //Operador de concatenação.
