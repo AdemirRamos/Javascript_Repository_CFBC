3ecb869005274d44871abb1f3036a826d06d9095
AdemirRamos <ademirramos634@gmail.com>
2022-09-13 12:46:32 -0300

    Adição da Aula 17

---
+++ CFB Cursos (2022)/Aula 17/aula17.js
@@ -0,0 +1,17 @@
+//Aula 17 - "Loop While" Em Javascript
+
+let número = 0
+
+while (número < 10) {
+	console.log(número += 1)
+}
+
+let num = 5
+let fatorial = 1
+
+while (num >= 1) {
+	fatorial *= num
+	num--
+}
+
+console.log(fatorial)
