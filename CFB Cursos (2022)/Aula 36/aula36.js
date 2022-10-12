ba98fd23c47d3feb43283952dbc2df1740bc1c33
AdemirRamos <ademirramos634@gmail.com>
2022-10-12 17:26:12 -0300

    Adição da Aula 36

---
+++ CFB Cursos (2022)/Aula 36/aula36.js
@@ -0,0 +1,31 @@
+//Aula 36 - Praticando "addEventListener" - Eventos Em Javascript (Parte 6)
+ 
+ const caixa_1 = document.querySelector('#caixa1')
+ const caixa_2 = document.getElementById('caixa2')
+ const botão = document.querySelector('#botão_transferir')
+ const cursos = [...document.querySelectorAll('.curso')]
+
+cursos.map((e) => {
+	e.addEventListener('click', (e) => {
+		const elemento = e.target
+		elemento.classList.toggle('selecionado')
+
+		console.log(elemento)
+	})
+})
+
+botão.addEventListener('click', () => {
+	const selecionado = [...document.querySelectorAll('.selecionado')]
+
+	//A chave ":not()." funciona para selecionar os elementos que não possuem a referência apresentada.
+
+	const não_selecionado = [...document.querySelectorAll('.curso:not(.selecionado)')]
+
+	selecionado.map((e) => {
+		caixa_2.appendChild(e)
+	})
+
+	não_selecionado.map((e) => {
+		caixa_1.appendChild(e)
+	})
+})
