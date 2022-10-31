0321d077abfb46f4ba887c85119380366bb806aa
AdemirRamos <ademirramos634@gmail.com>
2022-10-31 12:35:41 -0300

    Adição da Aula 45

---
+++ CFB Cursos (2022)/Aula 45/aula45.js
@@ -0,0 +1,120 @@
+//Aula 45 - Criando Elementos Antes e Depois De Outros Elementos
+
+const caixa_cursos = document.querySelector('#caixa_cursos')
+const botão = [...document.querySelectorAll('.curso')]
+const c1_2 = document.querySelector('#c1_2')
+const cursos = ['HTML', 'CSS', 'Javascript', 'PHP', 'React', 'MySQL', 'React Native']
+const botão_curso_selecionado = document.getElementById('botão_curso_selecionado')
+const botão_remover_curso = document.getElementById('botão_remover_curso')
+const botão_adicionar_antes = document.getElementById('adicionar_antes')
+const botão_adicionar_depois = document.getElementById('adicionar_depois')
+const nome_curso = document.getElementById('nome_curso_id')
+
+let índice = 0
+
+const criar_novo_curso = (curso) => {
+	const novo_elemento = document.createElement('div')
+	novo_elemento.setAttribute('id', 'c' + índice)
+	novo_elemento.setAttribute('class', 'curso c1')
+	novo_elemento.innerHTML = curso
+
+	const comandos = document.createElement('div')
+	comandos.setAttribute('class', 'comandos')
+
+	const rb = document.createElement('input')
+	rb.setAttribute('type', 'radio')
+	rb.setAttribute('name', 'rb_curso')
+
+	comandos.appendChild(rb)
+	novo_elemento.appendChild(comandos)
+
+	return novo_elemento
+}
+
+cursos.map((e, c) => {
+	const novo_elemento = criar_novo_curso(e)
+	caixa_cursos.appendChild(novo_elemento)
+	índice++
+})
+
+const radio_selecionado = () => {
+	const todos_radios = [...document.querySelectorAll('input[type=radio]')]
+
+	const radio_selecionado = todos_radios.filter((e, i, a) => {
+		return e.checked
+	})
+
+	return radio_selecionado[0]
+}
+
+botão_curso_selecionado.addEventListener('click', (event) => {
+	const rs = radio_selecionado()
+
+	try {
+		const curso_selecionado = rs.parentNode.previousSibling.textContent
+		console.log(curso_selecionado)
+		alert(`Curso selecionado: ${curso_selecionado}.`)
+	}
+
+	catch (exception) {
+		alert('Nenhum curso foi selecionado.')
+	}
+})
+
+botão_remover_curso.addEventListener('click', (event) => {
+
+	const rs = radio_selecionado()
+
+	if (rs != undefined) {
+		const curso_selecionado = rs.parentNode.parentNode	
+		curso_selecionado.remove()
+	}
+
+	else {
+		alert('Nenhum curso foi selecionado.')
+	}
+})
+
+botão_adicionar_antes.addEventListener('click', (e) => {
+	const rs = radio_selecionado()
+
+	try {
+
+		if (nome_curso.value != '') {
+			const curso_selecionado = rs.parentNode.parentNode
+			const novo_curso = criar_novo_curso(nome_curso.value)
+			caixa_cursos.insertBefore(novo_curso, curso_selecionado)
+			alert(`Curso selecionado: ${curso_selecionado}.`)
+		}
+
+		else {
+			alert('Digite o nome do curso.')
+		}
+	}
+
+	catch (exception) {
+		alert('Nenhum curso foi selecionado.')
+	}
+})
+
+botão_adicionar_depois.addEventListener('click', (e) => {
+	const rs = radio_selecionado()
+
+	try {
+
+		if (nome_curso.value != '') {
+			const curso_selecionado = rs.parentNode.parentNode
+			const novo_curso = criar_novo_curso(nome_curso.value)
+			caixa_cursos.insertBefore(novo_curso, curso_selecionado.nextSibling)
+			alert(`Curso selecionado: ${curso_selecionado}.`)
+		}
+
+		else {
+			alert('Digite o nome do curso.')
+		}
+	}
+
+	catch (exception) {
+		alert('Nenhum curso foi selecionado.')
+	}
+})
